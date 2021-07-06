37316e30d7ad993bb041a971e72df00cd896358b
ben <akunnyabens@gmail.com>
2021-07-06 23:30:44 +0700

    Database register + validasi email done v1.3

--- app/src/main/java/com/example/benfintechresponsi/RegisterActivity.kt
+++ app/src/main/java/com/example/benfintechresponsi/RegisterActivity.kt
@@ -4,9 +4,14 @@ import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.etemail
+import kotlinx.android.synthetic.main.activity_register.etpassword
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +22,22 @@ class RegisterActivity : AppCompatActivity() {
         var db = helper.readableDatabase
         var rs = db.rawQuery("SELECT * FROM USERS", null)
 
-        if (rs.moveToNext())
-            Toast.makeText(applicationContext, rs.getString(1), Toast.LENGTH_LONG).show()
+        etemail.addTextChangedListener(object:TextWatcher  {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if(android.util.Patterns.EMAIL_ADDRESS.matcher(etemail.text.toString()).matches())
+
+                else{
+                    etemail.setError("Email Salah")
+                }
+            }
+
+        })
 
         btregister.setOnClickListener{
             var cv = ContentValues()
