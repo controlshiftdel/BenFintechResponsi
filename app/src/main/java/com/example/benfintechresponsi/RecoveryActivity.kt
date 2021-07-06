6610a0b46c6907f4841dadddc694922cfd23f6bf
ben <akunnyabens@gmail.com>
2021-07-06 20:46:00 +0700

    Register + Recovery onclick done

--- app/src/main/java/com/example/benfintechresponsi/RecoveryActivity.kt
+++ app/src/main/java/com/example/benfintechresponsi/RecoveryActivity.kt
@@ -1,11 +1,19 @@
 package com.example.benfintechresponsi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RecoveryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recovery)
+
+        val pencet = findViewById<Button>(R.id.btsendemail)
+        pencet.setOnClickListener {
+            val lojin= Intent(this, LoginActivity::class.java)
+            startActivity(lojin)
+        }
     }
 }
--- app/src/main/java/com/example/benfintechresponsi/RegisterActivity.kt
+++ app/src/main/java/com/example/benfintechresponsi/RegisterActivity.kt
@@ -1,11 +1,19 @@
 package com.example.benfintechresponsi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val pencet = findViewById<Button>(R.id.btregister)
+        pencet.setOnClickListener {
+            val lojin= Intent(this, HomeActivity::class.java)
+            startActivity(lojin)
+        }
     }
 }
