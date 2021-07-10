c80480e3cd625a9debbb31dae3440e021fdb2a5a
ben <akunnyabens@gmail.com>
2021-07-10 23:58:40 +0700

    Final beresin profile validation  V2.5

--- app/src/main/java/com/example/benfintechresponsi/PaymentActivity.kt
+++ app/src/main/java/com/example/benfintechresponsi/PaymentActivity.kt
@@ -3,7 +3,9 @@ package com.example.benfintechresponsi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_payment.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class PaymentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,18 @@ class PaymentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_payment)
 
         btpay.setOnClickListener{
+            validatepay()
             val lojin= Intent(this, HomeActivity::class.java)
             startActivity(lojin)
+        }
+    }
+    fun validatepay() : Boolean{
+        if(etpay.text.length == 0){
+            Toast.makeText(this, "Harap Mengisi kode ", Toast.LENGTH_LONG).show()
+            return false
+        } else {
+
+            return true
         }
     }
 }
