61be2798444b820a7d35703dc24042fc90251331
ben <akunnyabens@gmail.com>
2021-07-10 15:19:25 +0700

    Update history fragment

---
+++ app/src/main/java/com/example/benfintechresponsi/isisetting/BankActivity.kt
@@ -0,0 +1,12 @@
+package com.example.benfintechresponsi.isisetting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.benfintechresponsi.R
+
+class BankActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bank)
+    }
+}
